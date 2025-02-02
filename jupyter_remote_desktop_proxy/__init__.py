a21aa2b6b94e21171bc6d59ca2a6636741b12891
datacabinet <pankaj@datacabinet.systems>
2025-02-02 14:56:28 -0800

    Update __init__.py to add correct handler paths

--- jupyter_remote_desktop_proxy/__init__.py
+++ jupyter_remote_desktop_proxy/__init__.py
@@ -1,9 +1,8 @@
 import os
-
 from .server_extension import load_jupyter_server_extension
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-
+PARENT = os.path.dirname(HERE)
 
 def _jupyter_server_extension_points():
     """
@@ -11,6 +10,17 @@ def _jupyter_server_extension_points():
     """
     return [{"module": "jupyter_remote_desktop_proxy"}]
 
+def _jupyter_server_config_dir():
+    """
+    Return the location of jupyter-server configuration files
+    """
+    return os.path.join(PARENT, 'jupyter-config', 'jupyter_server_config.d')
+
+def _jupyter_notebook_config_dir():
+    """
+    Return the location of jupyter-notebook configuration files
+    """
+    return os.path.join(PARENT, 'jupyter-config', 'jupyter_notebook_config.d')
 
 # For backward compatibility
 _load_jupyter_server_extension = load_jupyter_server_extension
